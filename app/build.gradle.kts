c1d1d3cfa28b065be0a37ac5c8af1a2ab19c0c8e
JungYoonLee <37658906+ds1mbp5720@users.noreply.github.com>
2026-04-03 21:09:19 +0900

    feat: Add Google Material Components
    Integrates Google Material Components to leverage their styling and UI elements, enhancing the app's visual design.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,6 +64,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.google.material)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.navigation.compose)
     
